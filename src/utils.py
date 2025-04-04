28c1ede9da04aaca31438f39e59426ff77a588b4
kathling <talk2keling@gmail.com>
2025-04-04 10:44:44 -0400

    clean up files

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,10 @@
+'''
+This file handles loading the dataset and generating the training data for the GamutMLP model.
+The main function is generate_normalized_training_data which samples pixels from the original ProPhoto image
+and applies normalization and encoding to retrieve a 120D feature vector for each pixel.
+The training data is then used to train the GamutMLP model.
+'''
+
 import torch
 import numpy as np
 
@@ -6,6 +13,64 @@ device = 'cpu'
 if (torch.cuda.is_available()):
     device = torch.device('cuda')
 
+# =================================================================================================
+# Functions for Gamut Reduction
+# =================================================================================================
+def gamma_encoding_for_sRGB(linear_img):
+    # https://en.wikipedia.org/wiki/SRGB#Transfer_function_(%22gamma%22) 
+    return torch.where(linear_img <= 0.0031308, 12.92 * linear_img, 1.055 * torch.pow(linear_img, 1/2.4) - 0.055)
+
+    # Alternate: https://colour.readthedocs.io/en/latest/generated/colour.cctf_encoding.html
+    # return colour.cctf_encoding(linear_img, function='sRGB')
+
+def gamma_decoding_for_sRGB(gamma_img):
+    # https://en.wikipedia.org/wiki/SRGB#Transfer_function_(%22gamma%22) 
+    return torch.where(gamma_img <= 0.04045, gamma_img / 12.92, ((gamma_img + 0.055) / 1.055) ** 2.4)
+
+    # Alternate: https://colour.readthedocs.io/en/latest/generated/colour.cctf_encoding.html
+    # return colour.cctf_decoding(gamma_img, function='sRGB')
+
+def gamut_reduction(I_PP, device):
+    # define M as described in the paper
+    I_PP = I_PP / 255.0  # ASSUMPTION: must normalize to floating pt format before computations (because we tried it without normalization and almost all values were clipped because they ranged from 0-255)
+    
+    # from page 10 of paper
+    M = torch.tensor([[2.0365, -0.7376, -0.2993],
+                      [-0.2257, 1.2232, 0.0027],
+                      [-0.0105, -0.1349, 1.1452]]).to(device)
+    unclipped_sRGB = torch.matmul(M, I_PP)
+
+    # transform unclipped sRGB such that in-gamut sRGB values are within range [0,1]
+    # locate out-of-gamut (OG) sRGB values, row-wise
+    # True = OG, False = in-gamut (IG)
+    R_mask = (unclipped_sRGB[0] < 0) | (unclipped_sRGB[0] > 1)
+    G_mask = (unclipped_sRGB[1] < 0) | (unclipped_sRGB[1] > 1)
+    B_mask = (unclipped_sRGB[2] < 0) | (unclipped_sRGB[2] > 1)
+    
+    # ASSUMPTION: a pixel is considered OG if any one of its colour values is out of [0,1] range
+    # we assume this is the correct assumption because Figures 2 and 3 show "clipped values" as (R', G', B') --> signifies all 3 color values of a pixel are clipped
+    # True = Pixel is OG, False = Pixel is in-gamut (IG)
+    OG_mask = torch.logical_or(R_mask, torch.logical_or(G_mask, B_mask))
+    OG_mask = torch.stack((OG_mask, OG_mask, OG_mask))
+
+    # clip, no values outside of [0,1] range
+    clipped_sRGB = torch.clamp(unclipped_sRGB, min=0, max=1)
+    
+    # gamma encoding
+    I_sRGB = gamma_encoding_for_sRGB(clipped_sRGB)
+    
+    # Equation (2)
+    M_inverse = torch.linalg.inv(M)
+    de_gamma_I_sRGB = gamma_decoding_for_sRGB(I_sRGB)
+
+    I_ClippedPP = torch.matmul(M_inverse, de_gamma_I_sRGB)
+    
+    return I_PP, I_ClippedPP, OG_mask
+
+
+# =================================================================================================
+# Functions to generate training data
+# =================================================================================================
 def generate_normalized_training_data(I_PP, I_ClippedPP, OG_mask):
     # Step 1: Sampling pixels from the original PP image and generate ground truth
     coords_5D, ground_truth = sampling(I_PP, I_ClippedPP, OG_mask)
@@ -17,7 +82,6 @@ def generate_normalized_training_data(I_PP, I_ClippedPP, OG_mask):
     train_data = encode(normalized_coords_5D)
 
     return train_data, ground_truth, coords_5D
-
 
 def sampling(I_PP, I_ClippedPP, OG_mask):
     '''
@@ -100,9 +164,8 @@ def normalize(all_unnormalized_5d_coords):
 
     return all_normalized_5d_coords
 
-
 def encodingFunctionGamma(z):
-    K = 12
+    K = 12          # number fixed in the paper
     result = []
     for i in range(0,K):
         result.append((torch.sin(2**i * torch.pi * z)))
@@ -122,11 +185,14 @@ def encode(all_normalized_5d_coords):
     
     return encoded_coords
 
-
-# Gamut Expansion Step, (just getting the 120D feature vectors for all pixels)
-# Method 2: Use display_I_PP, display_I_ClippedPP, display_OG_mask which are in display form with shape (512,512,3)
-# Assumption: using Method 2 (defined in utils.py sampling() method) due to ease in finding spatial coordinates
+# =================================================================================================
+# Functions for Gamut Expansion
+# =================================================================================================
 def gamut_expansion(I_ClippedPP):
+    # Gamut Expansion Step, (just getting the 120D feature vectors for all pixels)
+    # Method 2: Use display_I_PP, display_I_ClippedPP, display_OG_mask which are in display form with shape (512,512,3)
+    # Assumption: using Method 2 (defined in utils.py sampling() method) due to ease in finding spatial coordinates
+
     # ASSUMPTION: "all pixels" refers to all pixels in the image (not just out-of-gamut)
     # From page 3, gamut expansion section:
     # "The extracted model predicts the residuals of all pixels and adds them to the IClippedPP to recover the color values as shown in Figure 3." 
